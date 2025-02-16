239c9f9b8cbad708cff2eb5bc467760b30214612
Akshay Kumar <akshaykumar0780238@gmail.com>
2025-02-17 00:04:07 +0530

    Update userValidator.js

--- Server/middleware/userValidator.js
+++ Server/middleware/userValidator.js
@@ -23,20 +23,5 @@ const signupValidation = (req, res, next) => {
   next();
 };
 
-// const loginValidation = (req, res, next) => {
-//   const schema = joi.object({
-//     email: joi.string().email().required(),
-//     password: joi.string().min(5).max(20).required(),
-//   });
-
-//   const { error } = schema.validate(req.body);
-//   if (error) {
-//     return res.status(400).json({
-//       success: false,
-//       message: "Validation failed: " + error.details[0].message,
-//     });
-//   }
-//   next();
-// };
 
 module.exports = signupValidation
